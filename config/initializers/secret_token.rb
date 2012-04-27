0403fa66b63664521daaf7f7bb7321335be1910a
Brian Mulligan <shmully@yahoo.com>
2012-04-26 21:50:19 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Boathouse::Application.config.secret_token = '914388f55572c89c1d39a4d7504a5fa9db41be569371ea51f93231233b1841dc2537f295bcbe3a6b69816339ccc975639a345fb51074d7381194fe318e19bc16'
